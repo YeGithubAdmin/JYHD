4df84d8d6e840ac65181c5784fbaecc72465631c
YeGithubAdmin <565635562@qq.com>
2017-11-24 19:04:34 +0800

    后台客服

--- Application/Jy_admin/Controller/FeedBackController.class.php
+++ Application/Jy_admin/Controller/FeedBackController.class.php
@@ -9,13 +9,14 @@ class FeedBackController extends ComController {
         $page           = $this->page;              //页码
         $num            = $this->num;               //条数
         $time = strtotime(date('Y-m-d',time()));
-        $StartTime = date("Y-m-d",$time);
-        $EndTime   =  date("Y-m-d",$time+24*60*60);
+        $StartTime = date("Y-m-d",$time-15*60*60*24);
+        $EndTime   =  date("Y-m-d",$time);
         $search['datemin']                   =      I('param.datemin',$StartTime,'trim');        //下单时间
         $search['datemax']                   =      I('param.datemax',$EndTime,'trim');        //下单时间
         $search['playerid']                  =      I('param.playerid','','intval');         //用户ID
         $search['Channel']                   =      I('param.Channel','','trim');        //渠道
-        $search['Status']                    =      I('param.Status','','intval');         //状态
+        $search['Status']                    =      I('param.Status',0,'intval');         //状态
+        $search['Type']                    =      I('param.Type',0,'intval');         //状态
         $where = '1';
         //查询渠道
         $ChannelFiled = array(
@@ -28,32 +29,95 @@ class FeedBackController extends ComController {
             ->select();
         //渠道
         if ($search['Channel'] != ''){
-            $where .= ' and `PayChannel`="'.$search['Channel'].'"';
+            $where .= ' and `Channel`="'.$search['Channel'].'"';
         }
         //用户ID
-        if ($search['playerid'] != '' && $search['playerid'] != 0 ){
+        if ($search['playerid'] != 0 ){
             $where .= ' and `playerid`='.$search['playerid'];
+        }else{
+            $search['playerid'] = '';
         }
-        //支付状态
-        if ($search['Status'] != '' && $search['Status'] != 0 ){
+        //状态
+        if ($search['Status'] != 0 ){
             $where .= ' and `Status`='.$search['Status'];
         }
         //起始时间
         if ($search['datemin'] != ''){
-            $where .= ' and `FoundTime`>= str_to_date("'.$search['datemin'].'","%Y-%m-%d %H:%i:%s")';
+            $where .= ' and `DateTime`>= str_to_date("'.$search['datemin'].'","%Y-%m-%d %H:%i:%s")';
         }
         //结束时间
         if ($search['datemax'] != ''){
             $datemax = date("Y-m-d H:i:s",strtotime($search['datemax'])+24*60*60) ;
-            $where .= ' and `FoundTime` < str_to_date("'.$datemax.'","%Y-%m-%d %H:%i:%s")';
+            $where .= ' and `DateTime` < str_to_date("'.$datemax.'","%Y-%m-%d %H:%i:%s")';
         }
+        $count =  M('log_feed_back')
+                  ->where($where)
+                  ->count();
+        $Page       = new \Common\Lib\Page($count,$num);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+        $show       = $Page->show();// 分页显示输出
+        $catData = M('log_feed_back')
+                   ->where($where)
+                   ->field(array(
+                        'Id',
+                        'playerid',
+                        'Status',
+                        'PackageVersion',
+                        'TxQq',
+                        'Phone',
+                        'Channel',
+                        'VerSion',
+                        'UpDateTime',
+                        'DateTime',
+                   ))
+                   ->order('DateTime desc')
+                   ->limit($page*$num,$num)
+                   ->select();
+        $this->assign('page',$show);
         $this->assign('Channel',$Channel);
+        $this->assign('info',$catData);
         $this->assign('search',$search);
         $this->display('index');
     }
-    //物品列表
+    //
     public  function authority(){
-
+        $Id = I('param.Id',0,'intval');
+        $obj = new \Common\Lib\func();
+        $UserInfo = $this->userInfo;
+        if($Id<=0){
+            $obj->showmessage('非法操作');
+        }
+        $catData = M('log_feed_back')
+                   ->where('Id = '.$Id.' and  IsDel = 1')
+                   ->field(array(
+                       'Id',
+                       'playerid',
+                       'Status',
+                       'Type',
+                       'Fcontent',
+                       'Rcontent',
+                       'TxQq',
+                       'Phone',
+                       'DateTime',
+                   ))
+                   ->find();
+        if(IS_POST){
+            $Rcontent = I('param.Rcontent','','trim');
+            $db = M('log_feed_back');
+            $UpData = $db
+                      ->where('Id = '.$Id)
+                      ->save(array(
+                          'Rcontent'=>$Rcontent,
+                          'Status'=>2,
+                          'UpDateTime'=>date('Y-m-d H:i:s'),
+                          'UpName'=>$UserInfo['name'],
+                      ));
+            if($UpData){
+                $obj->showmessage('回复成功','/jy_admin/FeedBack/index');
+            }else{
+                $obj->showmessage('回复失败');
+            }
+        }
+        $this->assign('info',$catData);
         $this->display('authority');
     }
 }
