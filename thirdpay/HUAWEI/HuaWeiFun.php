b9dd0e6b5efa69ffedc08c4b93feaf8827ff356f
YeGithubAdmin <565635562@qq.com>
2017-12-28 19:05:59 +0800

    华为支付加签

--- thirdpay/HUAWEI/HuaWeiFun.php
+++ thirdpay/HUAWEI/HuaWeiFun.php
@@ -64,33 +64,30 @@ class  HuaWeiFun{
       return $valueMap;
   }
   //签名私钥
-  public  function redSignkey($param){
-      ksort($param);
+    public  function redSignkey($param){
+        ksort($param);
+        $arg = "";
+        while (list ($key, $val) = each ($param)) {
+            $arg.=$key."=".$val."&";
+        }
+        $Filename = HUAWEISDK.'Privatekey.pem';
+        if(!file_exists($Filename)){
+            return false;
+        }
+        //去掉最后一个&字符
+        $arg = substr($arg,0,count($arg)-2);
+        //如果存在转义字符，那么去掉转义
+        if(get_magic_quotes_gpc()){$arg = stripslashes($arg);}
 
-      $arg = "";
-      while (list ($key, $val) = each ($param)) {
-          $arg.=$key."=".$val."&";
-      }
-      $Filename = HUAWEISDK.'Privatekey.pem';
-      if(!file_exists($Filename)){
-        return false;
-      }
-
-      //去掉最后一个&字符
-      $arg = substr($arg,0,count($arg)-2);
-      var_dump($arg);die;
-      //如果存在转义字符，那么去掉转义
-      if(get_magic_quotes_gpc()){$arg = stripslashes($arg);}
-
-      $Private =   @file_get_contents($Filename);
-     $res = openssl_get_privatekey($Private);
-       //  $data=sha1($data); //sha1加密（如果需要的话，如果进行加密，则对方也要进行加密后做对比）
-      openssl_sign($arg, $Sign, $res,OPENSSL_ALGO_SHA256);
-      openssl_free_key($res);
-      //base64编码
-      $Sign = base64_encode($Sign);
-      return $Sign;
-  }
+        $Private =   @file_get_contents($Filename);
+        $res = openssl_get_privatekey($Private);
+        // $data=sha1($data); //sha1加密（如果需要的话，如果进行加密，则对方也要进行加密后做对比）
+        openssl_sign($arg, $Sign, $res);
+        openssl_free_key($res);
+        //base64编码
+        $Sign = base64_encode($Sign);
+        return $Sign;
+    }
 
 
   //Rsa
